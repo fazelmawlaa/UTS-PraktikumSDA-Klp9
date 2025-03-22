b682fcf29c8695049d5000d492a4e474953791b8
farisfud <fudhailifaris@gmail.com>
2025-03-22 23:40:02 +0700

    Update parsing_expression.c

--- parsing_expression.c
+++ parsing_expression.c
@@ -52,3 +52,14 @@ int priority(char c) {
     }
 }
 
+void strrev(char *str) {
+    int i, j;
+    char temp;
+    int len = strlen(str);
+    for (i = 0, j = len - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}
+
